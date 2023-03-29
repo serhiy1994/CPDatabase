ea445ea7fc75689020c6b739cf6964ea0a8a0536
Serhiy Mashkovskyi <mssss@ukr.net>
2023-03-29 22:15:49 +0300

    completed pagination 29-3-23

--- CPDatabase/Models/ViewModels/PageViewModel.cs
+++ CPDatabase/Models/ViewModels/PageViewModel.cs
@@ -14,6 +14,12 @@ namespace CPDatabase.Models
         }
 
         public bool HasPreviousPage { get { return (PageNumber > 1); } }
+        public bool HasPreviousPage2 { get { return (PageNumber > 2); } }
+        public bool HasPreviousPage3 { get { return (PageNumber > 3); } }
         public bool HasNextPage { get { return (PageNumber < TotalPages); } }
+        public bool HasNextPage2 { get { return (PageNumber < TotalPages - 1); } }
+        public bool HasNextPage3 { get { return (PageNumber < TotalPages - 2); } }
+        public bool TooFarToFirst { get { return (PageNumber - 1 > 3); } }
+        public bool TooFarToLast { get { return (TotalPages - PageNumber > 3); } }
     }
 }
